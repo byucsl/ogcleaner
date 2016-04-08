320a7e3bc9190c8a6d4e45492aacc4e642c7440b
M. Stanley Fujimoto <sfujimoto@gmail.com>
2016-04-07 18:16:00 -0600

    aligner added
    script is able to parse orthodb data and align using mafft.

--- README.md
+++ README.md
@@ -4,9 +4,34 @@
 
 ## Methodology
 
-## Required software
+## Installation/Required software
 
 1. python
 1. scikit-learn
 1. aliscore
 1. mafft
+
+## Tutorial
+
+1. Training a filtering model
+
+```bash
+# Get a dataset from OrthoDB.
+# This can be done via the OrthoDB website, or you can use wget if you know how to query their APIs
+wget "http://orthodb.org/fasta?query=&level=6656&species=6656&universal=1&singlecopy=0.9"
+
+# Run the model training script
+# This script will take care of everything for you after you have a dataset from OrthoDB, includeing:
+#   1. Parsing the sequences into their OrthoDB Groups
+#   2. Generate false-positive homology clusters from the true-positive homology clusters
+#   3. Align the clusters using MAFFT
+#   4. Featurize the clusters
+#   5. Train a filtering model
+bin/train_model.py
+```
+
+1. Filtering using a trained model
+
+## Citing this package
+
+## Acknowledgements
--- bin/train_model.py
+++ bin/train_model.py
@@ -1,26 +1,30 @@
 import sys, argparse, re, time, os
 from subprocess import call
 from pathlib import Path
+from multiprocessing import Pool, Lock, Queue
 
 
 # global variables
 version = "0.1a"
 p = Path( __file__ )
-default_mafft_path = str( p.resolve() ) + "/mafft"
+default_mafft_path = str( p.resolve().parents[ 1 ] ) + "/lib/mafft_bin/mafft"
+q = Queue()
 
 # TODO: enumerate all possible models and features
 available_models = "svm"
 available_features = "length"
 
-def segregate_orthodb_groups( fasta_file_path, group_dir ):
-    # check if the output directory exists
-    if not os.path.exists( group_dir ):
-        status = call( [ "mkdir", group_dir ] )
 
-        if status == 0:
-            errw( "Created orthogroup fasta folder " + group_dir + "\n" )
-        else:
-            sys.exit( "ERROR! Could not create the orthogroup fasta folder. Aborting!" )
+def init_child(lock_):
+    global lock
+    lock = lock_
+
+
+def remove_ambiguous_amino_acids( seq ):
+    return seq.replace( 'U', '' ).replace( 'B', '' ).replace( 'X', '' ).replace( 'Z', '' )
+
+
+def segregate_orthodb_groups( fasta_file_path, group_dir ):
 
     errw( "\tSegregating the sequences into their orthogroups..." )
     # read in the fasta file
@@ -62,7 +66,7 @@ def segregate_orthodb_groups( fasta_file_path, group_dir ):
                 cur_seq_header = line.strip()
                 cur_seq_seq = ""
             else:
-                cur_seq_seq += line.strip()
+                cur_seq_seq += remove_ambiguous_amino_acids( line.strip() )
         cur_group_seqs.append( cur_seq_header + "\n" + cur_seq_seq )
         with open( group_dir + "/" + cur_group + ".fasta", 'w' ) as out:
             group_names.append( cur_group )
@@ -75,32 +79,75 @@ def segregate_orthodb_groups( fasta_file_path, group_dir ):
         return group_names
 
 
-def align_cluster( aligner_path, aligner_opts, cluster_path ):
-    errw( "\t\tAligning " + cluster_path + "..." )
-    status = call( [ aligner_path, aligner_opts, 
-    if status != 0:
-        errw( "Alignment error!!\n" )
-    else:
-        errw( "Done!\n" )
+def align_cluster( aligner_path, aligner_opts, cluster_path, cluster_name, aligned_dir, logs_dir ):
+    align_out_file_path = aligned_dir + "/" + cluster_name
+    align_stderr_file_path = logs_dir + "/" + cluster_name + ".mafft_alignment.stderr.out"
+    
+    with open( align_out_file_path, 'w' ) as alignment_fh, open( align_stderr_file_path, 'w' ) as stderr_fh:
+        status = call( [ aligner_path, cluster_path ], stdout = alignment_fh, stderr = stderr_fh )
+        
+        with lock:
+            errw( "\t\tAligning " + cluster_path + "..." )
+            if status != 0:
+                errw( "Alignment error!!\n" )
+            else:
+                errw( "Done!\n" )
 
+def align_worker( item ):
+    aligner_path = item[ 0 ]
+    aligner_opts = item[ 1 ]
+    clusters_out_path = item[ 2 ]
+    cluster_name = item[ 3 ]
+    aligned_dir = item[ 4 ]
+    logs_dir = item[ 5 ]
+    align_cluster( aligner_path, aligner_opts, clusters_out_path, cluster_name, aligned_dir, logs_dir )
 
-def align_clusters( aligner_path, aligner_opts, clusters_dir, cluster_names  ):
+def align_clusters( aligner_path, aligner_opts, clusters_dir, cluster_names, aligned_dir, threads, logs_dir ):
     errw( "\tAligning clusters...\n" )
+
+    tasks = []
     for cluster_name in cluster_names:
-        align_cluster( aligner_path, aligner_opts, clusters_dir + "/" + cluster_name + ".fasta" )
-    errw( "Done!\n" )
+        tasks.append( ( aligner_path, aligner_opts, clusters_dir + "/" + cluster_name + ".fasta", cluster_name, aligned_dir, logs_dir ) )
+
+    lock = Lock()
+    pool = Pool( threads, initializer = init_child, initargs = ( lock, ) )
+    pool.map( align_worker, tasks )
+
+    pool.close()
+    pool.join()
+    errw( "\tDone aligning clusters!" )
 
 
 def errw( text ):
     sys.stderr.write( text )
 
+def dir_check( dir_path ):
+    # check if the output directory exists
+    if not os.path.exists( dir_path ):
+        status = call( [ "mkdir", dir_path ] )
+
+        if status == 0:
+            errw( "Created orthogroup fasta folder " + dir_path + "\n" )
+        else:
+            sys.exit( "ERROR! Could not create the directory " + dir_path + ". Aborting!" )
+
 
 def main( args ):
     errw( "OrthoClean model training module version " + version + "\n" )
+    errw( "Aligner: " + args.aligner_path + "\n" )
+    errw( "Aligner args: " + args.aligner_options + "\n" )
+    errw( "Beginning...\n" )
+
+    # param checking
+    ## check if the output directory exists
+    dir_check( args.orthodb_group_dir )
+    dir_check( args.aligned_dir )
+    dir_check( args.logs_dir )
+
     ortho_groups = segregate_orthodb_groups( args.orthodb_fasta, args.orthodb_group_dir )
 
     # align the ortho clusters
-    align_clusters( args.aligner_path, args.aligner_options, args.orthodb_group_dir, ortho_groups )
+    align_clusters( args.aligner_path, args.aligner_options, args.orthodb_group_dir, ortho_groups, args.aligned_dir, args.threads, args.logs_dir )
 
     # non-homology cluster generation
     ## sample sequences from homology clusters
@@ -110,10 +157,16 @@ def main( args ):
     # align the non-homology clusters
     align_clusters( args.nh_group_dir, nh_groups )
 
+    # featurize datasets
+
+    # train models
+
+    errw( "Finished!\n" )
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(
-            description = "Train models to clean putative orthology clusters."
+            description = "Train models to clean putative orthology clusters. Methodology published in Detecting false positive sequence homology: a machine learning approach, BMC Bioinformatics (24 February 2016, http://bmcbioinformatics.biomedcentral.com/articles/10.1186/s12859-016-0955-3). Please contact M. Stanley Fujimoto at sfujimoto@gmail.com for any questions.",
             )
     parser.add_argument( "--models",
             type = str,
@@ -141,12 +194,27 @@ if __name__ == "__main__":
     parser.add_argument( "--aligner_path",
             type = str,
             default = default_mafft_path,
-            help = "Default aligner is MAFFT and is set up during install. If you already have MAFFT or another aligner installed, provide the path here."
+            help = "Default aligner is MAFFT and is set up during install. If you already have MAFFT or another aligner installed, provide the path here. NOTE: this program is only designed to work with MAFFT, other aligners will take modification."
             )
     parser.add_argument( "--aligner_options",
             type = str,
             default = "",
             help = "Options for your aligner."
+            )
+    parser.add_argument( "--aligned_dir",
+            type = str,
+            default = "cluster_alignments",
+            help = "Directory to store all alignments."
+            )
+    parser.add_argument( "--threads",
+            type = int,
+            default = 1,
+            help = "Number of threads to use during alignment (Default 1 )."
+            )
+    parser.add_argument( "--logs_dir",
+            type = str,
+            default = "logs_dir",
+            help = "Directory to store progress output from programs."
             )
 
     args = parser.parse_args()
